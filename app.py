bd501727759593852e3fdb248c6ad68d7b138f2e
Aditya Madne <adityamadne26@gmail.com>
2026-06-05 14:36:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)   
 
 @app.route("/")
 def home():
-    return "Flask App Running on Cloud Run 🚀"
+    return "Flask working on Cloud Run 🚀"
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
