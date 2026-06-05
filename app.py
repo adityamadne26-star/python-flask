e9ed4fb48e0828ba206d04183fb5e06c77e5a9cd
Aditya Madne <adityamadne26@gmail.com>
2026-06-05 14:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return "Hello from Flask App"
+    return "Hello World from Flask CI/CD Pipeline"
 
 if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
